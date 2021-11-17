d0736bb6fe2c8cbe7a96cbe09221238d5ad125f9
Rodrigo Gonzalez <rodrigogonvaz@gmail.com>
2021-11-17 14:58:31 -0600

    practica de cerveza, no es el LAB

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const app			= express()
 
 const hbs			= require("hbs")
 
-require("dotenv").config()
+const PunkAPIWrapper	= require("punkapi-javascript-wrapper")
+const punkAPI			= new PunkAPIWrapper()
 
+require("dotenv").config()
 
 // 2. MIDDLEWARES
 app.use(express.static("public"))
@@ -17,11 +19,47 @@ hbs.registerPartials(__dirname + "/views/partials")
 
 // 3. RUTAS
 
-app.get("/", (req,res) => {
-	res.render("home")
+
+app.get("/random-beer", (req, res) => {
+    const randomBeers = punkAPI.getRandom()
+    randomBeers
+    .then((beers)=>{
+        res.render("random-beer",{
+            cheve:beers 
+        })
+    })
+})
+
+app.get("/beers", (req, res) => {
+
+	// PROMESAS
+	// ES UNA FUNCIÓN QUE VA A EJECUTAR UN PROCESO ASÍNCRONO (P.E. LA CONEXIÓN A UN SERVIDOR EXTERNO) Y DEPENDIENDO DEL RESULTADO SE VA A EJECUTAR OTRA FUNCIÓN.
+	
+	// OBTENIENDO LA PROMESA
+	const listBeers = punkAPI.getBeers()
+	console.log(listBeers)
+
+	// EVALUANDO LA PROMESA
+	listBeers
+		.then((beers) => { // SI LA PROMESA SE CUMPLIÓ EXITOSAMENTE
+            
+            console.log(beers)
+
+            res.render("beers", {  // hasta que no regresa la solicitud, no puedo hacer res - responder
+				data: beers
+			})   
+
+		})
+		.catch((error) => { // SI LA PROMESA NO SE CUMPLIÓ. ROMPISTE MI CORAZÓN.
+			console.log(error)
+		})
+	
 })
 
 
+app.get("/", (req,res) => {
+	res.render("home")
+})
 
 // 4. SERVIDOR
 
